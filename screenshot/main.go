22bbdc1f9f1077db551c63bd1ef98a3f9f901a49
mniyk <my.name.is.yohei.kono@gmail.com>
2023-07-23 01:28:00 +0900

    Add: add screenshot code

---
+++ screenshot/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"image/png"
+	"os"
+	"time"
+
+	"github.com/kbinani/screenshot"
+)
+
+func main() {
+	now := time.Now()
+	tm := now.Format("20060102150405")
+
+	if err := os.Mkdir(tm, 0777); err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+
+	n := screenshot.NumActiveDisplays()
+
+	for i := 0; i < n; i++ {
+		bounds := screenshot.GetDisplayBounds(i)
+
+		img, err := screenshot.CaptureRect(bounds)
+		if err != nil {
+			panic(err)
+		}
+
+		x := bounds.Dx()
+		y := bounds.Dy()
+
+		fn := fmt.Sprintf("%s/%d_%dx%d.png", tm, i, x, y)
+		f, _ := os.Create(fn)
+
+		png.Encode(f, img)
+
+		f.Close()
+	}
+}

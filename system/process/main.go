b9c19727fd68ae4c7e4eadc91f3f1002d413c45a
mniyk <my.name.is.yohei.kono@gmail.com>
2023-07-23 02:46:33 +0900

    Add: add process code

---
+++ system/process/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+func main() {
+	handle, err := syscall.CreateToolhelp32Snapshot(syscall.TH32CS_SNAPPROCESS, 0)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+	defer syscall.CloseHandle(handle)
+
+	var pe32 syscall.ProcessEntry32
+	pe32.Size = uint32(unsafe.Sizeof(pe32))
+	if err := syscall.Process32First(handle, &pe32); err != nil {
+		fmt.Printf("Error: %s", err)
+	}
+
+	for {
+		fmt.Printf(
+			"%d: %s\n",
+			pe32.ProcessID,
+			syscall.UTF16ToString(pe32.ExeFile[:]))
+		if err := syscall.Process32Next(handle, &pe32); err != nil {
+			break
+		}
+	}
+}
